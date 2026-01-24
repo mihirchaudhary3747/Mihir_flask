43c0331be8640c3927382d712cb2dbd00ed06455
mihirchaudhary3747 <156599916+mihirchaudhary3747@users.noreply.github.com>
2026-01-24 18:47:03 +0000

    added motivation web page with responsive bootstrap

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   return render_template('about.html')
+@app.route('/motivation')
+def mtv():
+  return render_template('motivation.html')
+@app.route("/resume")
+def resumepd():
+  return render_template('resume.hmtl')
 
 
 if __name__ == "__main__":
